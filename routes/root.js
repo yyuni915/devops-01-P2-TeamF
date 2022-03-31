08cdb0883a349767827f92a07ca25bb1945bf576
madame3369 <ghwicheon@gmail.com>
2022-03-31 11:20:57 +0900

    feat: fastify index.js commit

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,8 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+    reply.send("Hello world!")
+         .code(200)
+         
   })
 }
